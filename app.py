3aeef82e8f2bc813230b70a427c36e24217fb237
VPyatakov <victor.pyatakov95@yandex.ru>
2021-04-14 18:28:43 +0300

    fix bug:

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from urls import urls
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-user = os.getenv('POSTGRES_USER')
-password = os.getenv('POSTGRES_PASSWORD')
-host = os.getenv('POSTGRES_HOST')
-port = os.getenv('POSTGRES_PORT')
-data_base_name = os.getenv('POSTGRES_DB')
 
 app = Flask(__name__)
 app.register_blueprint(urls)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}:{port}/{data_base_name}'
+database = 'database'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://testdbuser:testdbpass@{database}:5432/testdb'
 db = SQLAlchemy(app)
 
 if __name__ == "__main__":
